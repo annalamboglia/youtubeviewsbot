68b3c79bd9462d258263cca1f6577779ad23264d
Anna Lamboglia <62296543+annalamboglia@users.noreply.github.com>
2021-08-15 23:08:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -73,11 +73,11 @@ def doViews(link,id):
 	driver=webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
 	
 	for i in range(0,999):
-	 bot.sendMessage(id,"Apro il browser")
-	 driver.get(url)
-	 bot.sendMessage(id,"Ho aperto il Browser")
-	 time.sleep(300)
-	 bot.sendMessage(id,"ho aperto i browser ora aspetto")
+	  bot.sendMessage(id,"Apro il browser")
+	  driver.get(url)
+	  bot.sendMessage(id,"Ho aperto il Browser")
+	  time.sleep(300)
+	  bot.sendMessage(id,"ho aperto i browser ora aspetto")
 	driver.close()
 
 
